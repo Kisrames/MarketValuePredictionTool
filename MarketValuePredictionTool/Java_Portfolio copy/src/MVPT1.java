b5b99b17bc5e3587f276f27f68e2f439387f4774
Kishordaran Rames <kisrames@gmail.com>
2023-02-04 16:19:03 +0000

    Add files via upload

---
+++ MarketValuePredictionTool/Java_Portfolio copy/src/MVPT1.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class MVPT1 {
+    static int x ;
+    static double array1[];
+    static double array2[][];
+    static double y[];
+
+    static void Calculation (){
+        double sum = 0;
+        int i = 0,j = 0;
+        for (i=0; i<x;i++){
+            for (j=0; j<x;j++){
+                y[j] = array1[j] * array2[j][i];
+                sum = sum + y[j];
+                if(j == x-1) {
+                    System.out.println(sum);
+                }
+            }
+            sum=0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner io = new Scanner(System.in);
+        System.out.println("Please enter the number of comparatives : ");
+        x = io.nextInt();
+        array1 = new double[x];
+        array2 = new double[x][x];
+        y = new double [x];
+        for(int k = 0; k < x; k++){
+            System.out.printf("Please enter the %d divided value: \n", k);
+            array1[k] = io.nextDouble();
+        }
+        for(int r = 0; r < x; r++) {
+            for (int c = 0; c < x; c++) {
+                System.out.println("Please Enter Input : ");
+                array2[r][c] = io.nextDouble();
+            }
+        }
+        Calculation ();
+    }
+}
